8876093f498ef95ba87827a23468feb174a15bc9
Adam Lee <adamlarsonlee@gmail.com>
2025-09-12 16:42:58 -0500

    fix latest tag

--- src/wallet/wallet.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { WalletResolver } from './wallet.resolver';
-import { WalletService } from './wallet.service';
-
-@Module({
-  providers: [WalletResolver, WalletService],
-})
-export class WalletModule {}
